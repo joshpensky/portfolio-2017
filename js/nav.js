a29d8d95498f33624fae3246ffb8f35bab06a6c4
Josh Pensky <joshuapensky@gmail.com>
2017-07-05 14:42:12 -0400

    Added navigation reactivity to scrolling, robots.txt, humans.txt

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,18 @@
 var lastPos = 0;
 var hidden = false;
 
+/**
+ * Sets the last position to the window's current y-position on load.
+ */
 window.addEventListener("load", () => {
     lastPos = window.scrollY;
 });
 
+/**
+ * Manipulates the hidden variable on scroll based on vertical scroll position.
+ * @see {@link navHider}
+ * @see {@link navOverlay}
+ */
 window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
         if (lastPos >= window.scrollY) {
@@ -16,23 +24,31 @@ window.addEventListener("scroll", () => {
     } else {
         hidden = false;
     }
-    //navHider();
+    navHider();
+    navOverlay();
 });
 
+/**
+ * Hides the nav bar if the hidden variable is true, or shows if false;
+ */
 function navHider() {
     var navBar = document.querySelector("nav");
     if (hidden) {
-        navBar.classList.add("nav__hidden");
+        navBar.classList.add("nav--hidden");
     } else {
-        navBar.classList.remove("nav__hidden");
+        navBar.classList.remove("nav--hidden");
     }
 }
 
+/**
+ * Adds the overlay class to the nav bar if the Y-scroll is greater than 50,
+ * or removes it if less/equal to.
+ */
 function navOverlay() {
     var navBar = document.querySelector("nav");
     if (window.scrollY > 50) {
-        navBar.classList.add("nav__overlay");
+        navBar.classList.add("nav--overlay");
     } else {
-        navBar.classList.remove("nav__overlay");
+        navBar.classList.remove("nav--overlay");
     }
 }
