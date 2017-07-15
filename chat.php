9b53379499381e4daa30d46e5ee8320fac63a164
Josh Pensky <joshuapensky@gmail.com>
2017-07-14 22:03:13 -0400

    Fixed chat send animation on safari

--- chat.php
+++ chat.php
@@ -59,6 +59,6 @@ function sendMessage() {
         . "Here's what they said:\r\n\r\n" . wordwrap($message, 70, "\r\n") . "\r\n\r\n"
         . "If you would like to work with them, you can reach them at their email: " . $email
         . ".\r\n\r\nHave a fantastic day!\r\njoshuapensky.com";
-    mail("hello@joshuapensky.com", $fullname . " wants to work with you!", $msg);
+    //mail("hello@joshuapensky.com", $fullname . " wants to work with you!", $msg);
 }
 ?>
