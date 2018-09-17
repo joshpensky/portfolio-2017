0318d9070db852bcc8637c96411e5848fa4787db
Josh Pensky <joshuapensky@gmail.com>
2018-09-17 02:00:21 -0400

    Added gulp in order to create pages at buildtime, html partialsgit add -A

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+'use strict';
+
+var fs = require('fs');
+var gulp = require('gulp');
+var fileinclude = require('gulp-file-include');
+var mergeStream = require('merge-stream');
+var rename = require('gulp-rename');
+var sequence = require('gulp-sequence');
+var sass = require('gulp-sass');
+var template = require('gulp-template');
+
+gulp.task('build', sequence('projects', 'html', 'sass', 'data'));
+
+gulp.task('rebuild', sequence('build', 'copy'))
+
+gulp.task('html', function() {
+  return gulp
+    .src('./src/pages/**/*')
+    .pipe(fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('sass', function() {
+    return gulp
+        .src('./src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('sass:watch', function() {
+    gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+gulp.task('projects', function() {
+    const projects = JSON.parse(fs.readFileSync('./src/data/project-data.json'));
+    var tasks = [];
+    for (var i = 0; i < projects.length; i++) {
+        var project = projects[i];
+        if (i - 1 >= 0) {
+            var previous = projects[i - 1];
+            project.previous = {
+                id: previous.id,
+                title: previous.title,
+            }
+        }
+        if (i + 1 < projects.length) {
+            var next = projects[i + 1];
+            project.next = {
+                id: next.id,
+                title: next.title,
+            }
+        }
+        tasks.push(
+            gulp.src('./src/templates/project.html')
+                .pipe(rename(project.id + '.html'))
+                .pipe(template(project))
+                .pipe(gulp.dest('./src/pages/projects'))
+        );
+    }
+    return mergeStream(tasks);
+})
+
+gulp.task('data', function() {
+    return gulp
+        .src('./src/data/**/*')
+        .pipe(gulp.dest('./dist/data'))
+});
+
+gulp.task('copy', function() {
+    return gulp
+        .src([
+            './src/fonts/**/*',
+            './src/img/**/*',
+            './src/js/**/*',
+            './src/php/**/*',
+            './src/humans.txt',
+            './src/resume.pdf',
+            './src/robots.txt'
+        ], {
+            base: './src'
+        })
+        .pipe(gulp.dest('./dist'));
+});
